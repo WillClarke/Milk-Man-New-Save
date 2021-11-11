f438ae750f01e4a838ac3f982a679eaa32f9eda2
WillClarke <520379@chrome.escg.ac.uk>
2021-11-11 17:49:23 +0000

    initial commit

---
+++ Assets/Script/PlayerAfterImage.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:506b4117d0b2b9c13d148d224830d72be033bf61b5f7b57b6ab36db42675c4df
+size 659
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bc86a63c8d9b936450da43485951d0640f114107da1127de0d6d91209bfb04e2
+size 10463
