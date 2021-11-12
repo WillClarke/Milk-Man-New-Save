283e0d4b0bba3f148be357aee5c527c510a8d334
WillClarke <520379@chrome.escg.ac.uk>
2021-11-12 00:28:49 +0000

    Combat animations and attack script added

---
+++ Assets/Script/PlayerCombatController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6cf14dc8f0bec71ec0ae402c642e9ccca2658cb868e0d7cc6f36fc8a872a851b
+size 2403
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2dd54baf8c0592e1430a27603a074775ca842f91eef6c2f078fbdf59d5ea35f6
-size 11971
+oid sha256:400b7c428a903ac4035438f4c710e3002a61627d0bd0dc4d081665b380bd751e
+size 12118
