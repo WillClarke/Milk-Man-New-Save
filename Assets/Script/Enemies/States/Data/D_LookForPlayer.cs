cbcce4e7521bcaf2ba9209634db3996f7c5f5466
WillClarke <520379@chrome.escg.ac.uk>
2021-11-17 22:09:49 +0000

    Enemy looks for player script

---
+++ Assets/Script/Enemies/States/Data/D_LookForPlayer.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a31f176c716222bb5b0c46899b6a283820d44775c3e7f0fba897c5596ca9613
+size 331
---
+++ Assets/Script/Enemies/States/LookForPlayerState.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7abae972a6a43c1c49864b0ee6b1ff5d0a6d1fc4b9f31b2135639959e194f38e
+size 2051
